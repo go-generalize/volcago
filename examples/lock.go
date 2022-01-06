60599bb5bd5ed5e2e652dfc3f9bb5fc3aaf7fb26
54m <30588003+54m@users.noreply.github.com>
2022-01-06 14:42:53 +0900

    update: examples

--- examples/lock.go
+++ examples/lock.go
@@ -4,8 +4,14 @@ package examples
 
 // Lock - with automatic id generation
 type Lock struct {
-	ID   string             `firestore:"-" firestore_key:"auto"`
-	Text string             `firestore:"text" unique:""`
-	Flag map[string]float64 `firestore:"flag"`
+	ID     string             `firestore:"-" firestore_key:"auto"`
+	Text   string             `firestore:"text" unique:""`
+	Flag   map[string]float64 `firestore:"flag"`
+	Nested Nested             `firestore:"nested"`
 	Meta
+}
+
+// Nested - nested struct
+type Nested struct {
+	Name string `firestore:"name"`
 }
