47cf35bba6f5c1a3c2dc4095ca45f984391f866f
Tsuzu <8574909+tsuzu@users.noreply.github.com>
2022-01-13 15:33:18 +0900

    Generate testfiles, Add generator_test.go to generate testfiles w/o emulator

---
+++ generator/generator_test.go
@@ -0,0 +1,65 @@
+//go:build !emulator
+// +build !emulator
+
+package generator
+
+import (
+	"os"
+	"os/exec"
+	"path/filepath"
+	"testing"
+)
+
+func execTest(t *testing.T) {
+	t.Helper()
+
+	b, err := exec.Command("go", "test", "./tests", "-v", "-tags", "internal").CombinedOutput()
+
+	if err != nil {
+		t.Fatalf("go test failed: %+v(%s)", err, string(b))
+	}
+}
+
+func run(t *testing.T, structName string, subCollection bool) {
+	t.Helper()
+
+	gen, err := NewGenerator(".")
+
+	if err != nil {
+		t.Fatalf("NewGenerator failed: %+v", err)
+	}
+
+	opt := NewDefaultGenerateOption()
+	opt.Subcollection = subCollection
+
+	if err := gen.Generate(structName, opt); err != nil {
+		t.Fatalf("Generate failed: %+v", err)
+	}
+}
+
+func TestGenerator(t *testing.T) {
+	root, err := os.Getwd()
+
+	if err != nil {
+		t.Fatalf("failed to getwd: %+v", err)
+	}
+
+	t.Run("AutomaticIDGeneration", func(tr *testing.T) {
+		if err := os.Chdir(filepath.Join(root, "testfiles/auto")); err != nil {
+			tr.Fatalf("chdir failed: %+v", err)
+		}
+
+		run(t, "Task", false)
+		run(t, "Lock", false)
+	})
+
+	t.Run("IDSpecified", func(tr *testing.T) {
+		if err := os.Chdir(filepath.Join(root, "testfiles/not_auto")); err != nil {
+			tr.Fatalf("chdir failed: %+v", err)
+		}
+
+		run(t, "Task", false)
+
+		run(t, "SubTask", true)
+	})
+}
